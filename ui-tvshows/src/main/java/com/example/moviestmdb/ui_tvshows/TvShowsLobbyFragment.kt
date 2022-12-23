bad9757235d6b80db973925230372544fd94410c
Elna <ederech@digibank.co.il>
2022-12-23 14:04:04 +0200

    add compose

--- ui-tvshows/src/main/java/com/example/moviestmdb/ui_tvshows/TvShowsLobbyFragment.kt
+++ ui-tvshows/src/main/java/com/example/moviestmdb/ui_tvshows/TvShowsLobbyFragment.kt
@@ -4,10 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviestmdb.core.TmdbImageManager
+import com.example.moviestmdb.core.extensions.launchAndRepeatWithViewLifecycle
+import com.example.moviestmdb.core_ui.util.SpaceItemDecoration
 import com.example.moviestmdb.ui_tvshows.databinding.FragmentTvshowsLobbyBinding
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class TvShowsLobbyFragment : Fragment() {
+
+    private val viewModel: TvShowsLobbyViewModel by viewModels()
+
+//    lateinit var popularMoviesAdapter: PopularMoviesCarrouselAdapter
+//    lateinit var topRatedMoviesAdapter: PopularMoviesCarrouselAdapter
+//    lateinit var upcomingMoviesAdapter: PopularMoviesCarrouselAdapter
+//    lateinit var nowPlayingMoviesAdapter: PopularMoviesCarrouselAdapter
+
+    @Inject
+    lateinit var tmdbImageManager: TmdbImageManager
 
 
     lateinit var binding: FragmentTvshowsLobbyBinding
@@ -17,7 +40,26 @@ class TvShowsLobbyFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentTvshowsLobbyBinding.inflate(inflater, container, false)
-        return binding.root
+        return ComposeView(inflater.context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            setContent {
+//                val popularShowsState by viewModel.popularShowsState.collectAsState()
+//                val topRatedShowsState by viewModel.topRatedShowsState.collectAsState()
+//                val latestShowsState by viewModel.onTheAirShowsState.collectAsState()
+//
+//                TvShowsScreen(
+//                    popularShowsState = popularShowsState,
+//                    latestShowsState = latestShowsState,
+//                    topRatedShowsState = topRatedShowsState,
+//                    onTileClicked = { id ->
+//                        val bundle = bundleOf("id" to id)
+//                        findNavController().navigate(R.id.navigation_tv_show_details, bundle)
+//                    }
+//                )
+            }
+        }
     }
 }
---
+++ ui-tvshows/src/main/java/com/example/moviestmdb/ui_tvshows/TvShowsLobbyViewModel.kt
@@ -0,0 +1,128 @@
+package com.example.moviestmdb.ui_tvshows
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.moviestmdb.core.extensions.combine
+import com.example.moviestmdb.core.util.AppCoroutineDispatchers
+import com.example.moviestmdb.core.util.ObservableLoadingCounter
+import com.example.moviestmdb.core.util.UiMessageManager
+import com.example.moviestmdb.core.util.collectStatus
+import com.example.moviestmdb.domain.interactors.*
+import com.example.moviestmdb.domain.observers.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+
+@HiltViewModel
+class TvShowsLobbyViewModel @Inject constructor(
+    private val updatePopularMovies: UpdatePopularMovies,
+    private val updateUpcomingMovies: UpdateUpcomingMovies,
+    private val updateNowPlayingMovies: UpdateNowPlayingMovies,
+    private val updateTopRatedMovies: UpdateTopRatedMovies,
+    private val updateGeneres: UpdateGeneres,
+    observePopularMovies: ObservePopularMovies,
+    observeUpcomingMovies: ObserveUpcomingMovies,
+    observeNowPlayingMovies: ObserveNowPlayingMovies,
+    observeTopRatedMovies: ObserveTopRatedMovies,
+    private val dispatchers: AppCoroutineDispatchers,
+) : ViewModel() {
+
+    private val popularLoadingState = ObservableLoadingCounter()
+    private val topRatedLoadingState = ObservableLoadingCounter()
+    private val upcomingLoadingState = ObservableLoadingCounter()
+    private val nowPlayingLoadingState = ObservableLoadingCounter()
+    private val generesLoadingState = ObservableLoadingCounter()
+    private val uiMessageManager = UiMessageManager()
+
+    init {
+        observePopularMovies(ObservePopularMovies.Params(1))
+        observeUpcomingMovies(ObserveUpcomingMovies.Params(1))
+        observeNowPlayingMovies(ObserveNowPlayingMovies.Params(1))
+        observeTopRatedMovies(ObserveTopRatedMovies.Params(1))
+
+        refresh()
+    }
+
+    val state: StateFlow<LobbyViewState> = combine(
+        popularLoadingState.observable,
+        topRatedLoadingState.observable,
+        upcomingLoadingState.observable,
+        nowPlayingLoadingState.observable,
+        observePopularMovies.flow,
+        observeNowPlayingMovies.flow,
+        observeTopRatedMovies.flow,
+        observeUpcomingMovies.flow,
+        uiMessageManager.message,
+    ) { popularRefreshing, topRatedRefreshing, upcomingRefreshing, nowPlayingRefreshing,
+        popularMovies, nowPlayingMovies, topRatedMovies, upcomingMovies, message ->
+        LobbyViewState(
+            popularRefreshing = popularRefreshing,
+            topRatedRefreshing = topRatedRefreshing,
+            upcomingRefreshing = upcomingRefreshing,
+            nowPlayingRefreshing = nowPlayingRefreshing,
+            popularMovies = popularMovies,
+            topRatedMovies = topRatedMovies,
+            upcomingMovies = upcomingMovies,
+            nowPlayingMovies = nowPlayingMovies,
+            message = message
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        LobbyViewState.Empty
+    )
+
+
+    fun refresh() {
+        viewModelScope.launch(dispatchers.io) {
+            updatePopularMovies(UpdatePopularMovies.Params(UpdatePopularMovies.Page.REFRESH))
+                .collectStatus(
+                    popularLoadingState,
+                    uiMessageManager
+                )
+        }
+
+        viewModelScope.launch(dispatchers.io) {
+            updateUpcomingMovies(UpdateUpcomingMovies.Params(UpdateUpcomingMovies.Page.REFRESH))
+                .collectStatus(
+                    upcomingLoadingState,
+                    uiMessageManager
+                )
+        }
+
+        viewModelScope.launch(dispatchers.io) {
+            updateNowPlayingMovies(UpdateNowPlayingMovies.Params(UpdateNowPlayingMovies.Page.REFRESH))
+                .collectStatus(
+                    nowPlayingLoadingState,
+                    uiMessageManager
+                )
+        }
+
+        viewModelScope.launch(dispatchers.io) {
+            updateTopRatedMovies(UpdateTopRatedMovies.Params(UpdateTopRatedMovies.Page.REFRESH))
+                .collectStatus(
+                    topRatedLoadingState,
+                    uiMessageManager
+                )
+        }
+
+        viewModelScope.launch(dispatchers.io) {
+            updateGeneres(Unit).collect {
+                Timber.i("### Genere: $it")
+            }
+        }
+    }
+
+    fun clearMessage(id: Long) {
+        viewModelScope.launch {
+            uiMessageManager.clearMessage(id)
+        }
+    }
+}
